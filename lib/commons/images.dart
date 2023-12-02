257927bd6b8af58e648e7c583b5592f38f2ffba6
kaouther <126014958+kaouthergithub@users.noreply.github.com>
2023-12-02 17:51:39 +0100

    Add files via upload

---
+++ lib/commons/images.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+///////////////////////////////// Png Images //////////////////////////////////
+
+const firstcategory_image = "images/mobility_aids.png";
+const secondcategory_image = "images/diagnostic_equi.png";
+const thirdcategory_image = "images/med_supplies.png";
+const fourthcategory_image="images/medical_equipement.png";
+const unkown="images/unkown.png";
+const tryy="images/try.png";
