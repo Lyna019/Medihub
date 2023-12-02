870b5ac4ec0483b246f6dbb8452d061cb44499b9
kaouther <126014958+kaouthergithub@users.noreply.github.com>
2023-12-02 17:51:56 +0100

    Add files via upload

---
+++ lib/commons/styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+const categoryTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: textCategoryColor,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w700,
+  fontStyle: FontStyle.normal,
+);
+const paragraphTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: textCategoryColor,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w700,
+  fontStyle: FontStyle.normal,
+);
+
+const titleTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: titleCategoryColor,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w700,
+  fontStyle: FontStyle.normal,
+);
+const subTitleTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+const subTitleblueTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.blue,
+  fontWeight: FontWeight.bold,
+);
+const subSubTitleTextStyle = TextStyle(
+  fontSize: 15.0,
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+);
+const titleCategoryTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: titleCategoryColor,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w700,
+  fontStyle: FontStyle.normal,
+);
+const PriceTextStyle = TextStyle(
+  fontFamily: 'Roboto',
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+  color: Colors.blue,
+);
+////////////////////////////////////// review
+const reviewpersonnameTextStyle = TextStyle(
+  fontFamily: 'Roboto',
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+const reviewTextStyle = TextStyle(
+  fontFamily: 'Roboto',
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
